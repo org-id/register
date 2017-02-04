019394b06fe607b25447eabe67bf2fdc40aed1dc
Tim Davies <tim@timdavies.org.uk>
2017-02-04 07:21:00 +0000

    Creating a new structured schema for organisation identifier lists, providing listlists, and reworking existing organisation identifier entries. Includes script for converting future imports from AirTable.

---
+++ schema/update.py
@@ -0,0 +1,50 @@
+# This script updates the schema with new enumerations from the jurisdictions list
+import json
+import jsonmerge 
+import collections
+
+def update_schema(schema,filename,codelist,target,array=True):
+
+    enum = []
+    enum_titles = []
+
+    with open(filename) as file:
+        filedata = json.loads(file.read(),object_pairs_hook=collections.OrderedDict)
+        for code in filedata[codelist]:
+            enum.append(code['code'])
+            enum_titles.append(code['title']['en'])
+
+    if "/" in target:
+        if array:
+            schema['properties'][target.split("/")[0]]['properties'][target.split("/")[1]]['items']['enum'] = enum
+            schema['properties'][target.split("/")[0]]['properties'][target.split("/")[1]]['items']['options']['enum_titles'] = enum_titles
+        else:
+            schema['properties'][target.split("/")[0]]['properties'][target.split("/")[1]]['enum'] = enum
+            schema['properties'][target.split("/")[0]]['properties'][target.split("/")[1]]['options']['enum_titles'] = enum_titles
+    else:
+        if array:
+            schema['properties'][target]['items']['enum'] = enum
+            schema['properties'][target]['items']['options']['enum_titles'] = enum_titles
+        else:
+            schema['properties'][target]['enum'] = enum
+            schema['properties'][target]['options']['enum_titles'] = enum_titles
+
+    return schema
+
+
+with open("list-schema.json") as schema_file:
+    schema = json.loads(schema_file.read(),object_pairs_hook=collections.OrderedDict)
+
+
+schema = update_schema(schema,"codelist-jurisdictions.json","national","jurisdiction")
+schema = update_schema(schema,"codelist-jurisdictions.json","subnational","subnationalJurisdiction")
+schema = update_schema(schema,"codelist-structure.json","structure","structure")
+schema = update_schema(schema,"codelist-sector.json","sector","sector")
+schema = update_schema(schema,"codelist-listType.json","listType","listType",False) 
+schema = update_schema(schema,"codelist-availability.json","availability","data/availability")  
+schema = update_schema(schema,"codelist-features.json","features","data/features")  
+schema = update_schema(schema,"codelist-licenseStatus.json","licenseStatus","data/licenseStatus",False)
+
+with open("list-schema.json","w") as schema_file:
+    schema_file.write(json.dumps(schema,indent=4))
+    
