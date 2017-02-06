172b592e7340e2f4644e61e51456cd95d7bca7d8
Tim Davies <tim@timdavies.org.uk>
2017-02-06 10:35:30 +0000

    Draft of updated schema for Org ID list of lists

--- schema/update.py
+++ schema/update.py
@@ -3,39 +3,53 @@ import json
 import jsonmerge 
 import collections
 
-jur_list = ["XM","XI","XR","ZZ"]
-sub_list = []
-
-
-def get_codes(filename,codelist,target,array=True):
+def update_schema(schema,filename,codelist,target,array=True):
 
     enum = []
+    enum_titles = []
 
     with open(filename) as file:
         filedata = json.loads(file.read(),object_pairs_hook=collections.OrderedDict)
         for code in filedata[codelist]:
-            enum.append(code['code'])  
-    if array:
-        return {"properties": {target: {"items": {"enum":enum }}}}
+            enum.append(code['code'])
+            enum_titles.append(code['title']['en'])
+
+    if "/" in target:
+        if array:
+            schema['properties'][target.split("/")[0]]['properties'][target.split("/")[1]]['items']['enum'] = enum
+            schema['properties'][target.split("/")[0]]['properties'][target.split("/")[1]]['items']['options']['enum_titles'] = enum_titles
+        else:
+            schema['properties'][target.split("/")[0]]['properties'][target.split("/")[1]]['enum'] = enum
+            schema['properties'][target.split("/")[0]]['properties'][target.split("/")[1]]['options']['enum_titles'] = enum_titles
     else:
-        return {"properties": {target: {"enum":enum }}}
-
-
-jurisdiction = get_codes("codelist-jurisdictions.json","national","jurisdiction")
-subnational = get_codes("codelist-jurisdictions.json","subnational","subnationalJurisdiction")
-structure = get_codes("codelist-structure.json","structure","structure")
-sector = get_codes("codelist-sector.json","sector","sector")
-listType = get_codes("codelist-listType.json","listType","listType",False)
+        if array:
+            schema['properties'][target]['items']['enum'] = enum
+            schema['properties'][target]['items']['options']['enum_titles'] = enum_titles
+        else:
+            schema['properties'][target]['enum'] = enum
+            schema['properties'][target]['options']['enum_titles'] = enum_titles
+    
+    return schema
 
 
 with open("list-schema.json") as schema_file:
     schema = json.loads(schema_file.read(),object_pairs_hook=collections.OrderedDict)
 
-schema = jsonmerge.merge(schema,jurisdiction)
-schema = jsonmerge.merge(schema,subnational)
-schema = jsonmerge.merge(schema,structure)
-schema = jsonmerge.merge(schema,sector)
-schema = jsonmerge.merge(schema,listType)
+
+schema = update_schema(schema,"codelist-jurisdictions.json","national","jurisdiction")
+schema = update_schema(schema,"codelist-jurisdictions.json","subnational","subnationalJurisdiction")
+schema = update_schema(schema,"codelist-structure.json","structure","structure")
+schema = update_schema(schema,"codelist-sector.json","sector","sector")
+schema = update_schema(schema,"codelist-listType.json","listType","listType",False) 
+schema = update_schema(schema,"codelist-availability.json","availability","data/availability")  ## NEED TO UPDATE SCRIPT TO COPE WITH THIS
+schema = update_schema(schema,"codelist-features.json","features","data/features")  ## NEED TO UPDATE SCRIPT TO COPE WITH THIS
+schema = update_schema(schema,"codelist-licenseStatus.json","licenseStatus","data/licenseStatus",False)
+
+# schema = jsonmerge.merge(schema,jurisdiction)
+# schema = jsonmerge.merge(schema,subnational)
+# schema = jsonmerge.merge(schema,structure)
+# schema = jsonmerge.merge(schema,sector)
+# schema = jsonmerge.merge(schema,listType)
 
 print(json.dumps(schema,indent=4))
 
